bc93558332a5eb83f9be14cc714702e21c7d951d
hwshin <30830323+hex0713@users.noreply.github.com>
2026-03-30 21:06:37 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/[18108] 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // 1998 > 2541 (-543) 변환
+    
+    int transferNum = -543;
+    int year = 0;
+    cin >> year;
+    
+    cout << year + transferNum << endl;
+    return 0;
+}
