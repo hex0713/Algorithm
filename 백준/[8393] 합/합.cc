92e35214c48876087c333be0e84b85567bedb67f
hwshin <30830323+hex0713@users.noreply.github.com>
2026-04-02 21:32:00 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/[8393] 합/합.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n{0}, result{0};
+    
+    cin >> n;
+    
+    for(int i = 1; i <= n; i++)
+    {
+        result += i;
+    }
+
+    cout << result << endl;
+    
+    return 0;
+}
