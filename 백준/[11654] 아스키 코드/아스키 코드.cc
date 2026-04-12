7657dbc94e41b3a7342f96aa7d5ccca5ee3c9702
hwshin <30830323+hex0713@users.noreply.github.com>
2026-04-12 23:23:34 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/[11654] 아스키 코드/아스키 코드.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	
+	
+	char c{0};
+	
+	
+	cin >> c;
+	
+	cout << (int)c << "\n";
+
+
+	return 0;
+}
