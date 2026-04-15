031a64f43d3a11bc7b96887276f583ec1810db3b
hwshin <30830323+hex0713@users.noreply.github.com>
2026-04-16 07:24:06 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/[25083] 새싹/새싹.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	
+	cout << "         ,r\'\"7" << "\n";
+	cout << "r`-_   ,\'  ,/" << "\n";
+	cout << " \\. \". L_r\'" << "\n";
+	cout << "   `~\\/" << "\n";
+	cout << "      |" << "\n";
+	cout << "      |" << "\n";
+
+	
+	
+	return 0;
+}
