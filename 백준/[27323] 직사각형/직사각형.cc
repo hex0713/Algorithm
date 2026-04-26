082c0b8bacbe7049becc72f309cf69a29da741c0
hwshin <30830323+hex0713@users.noreply.github.com>
2026-04-26 15:01:46 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/[27323] 직사각형/직사각형.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int A{ 0 };
+	int B{ 0 };
+
+	cin >> A >> B;
+
+	cout << A * B << "\n";
+
+	return 0;
+}
