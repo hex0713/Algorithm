2d9caf1795b1dd0ea191f6364aa9e9c0f93eff30
hwshin <30830323+hex0713@users.noreply.github.com>
2026-04-29 21:12:28 +0900

    Title: 실수 받아 그대로 출력, Time: 0ms, Memory: 0MB, Status: Compile Error - Codetree

---
+++ 260429/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    float N{0f};
+
+    cout.precision(2);
+    cout << fixed;
+
+    cout << N << endl;
+
+    return 0;
+}
