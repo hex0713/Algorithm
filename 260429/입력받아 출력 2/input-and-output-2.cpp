887db6da47495f51923bbc771e06d67c572cef98
hwshin <30830323+hex0713@users.noreply.github.com>
2026-04-29 22:32:04 +0900

    Title: 입력받아 출력 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260429/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int front{0}, back{0};
+    char c;
+
+    cin >> front >> c >> back;
+
+    cout << front << back;
+    return 0;
+}
