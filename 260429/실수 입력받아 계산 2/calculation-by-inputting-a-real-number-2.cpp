1834cb92e0a5c553e98f3ffb6d87e55a62af2f6b
hwshin <30830323+hex0713@users.noreply.github.com>
2026-04-29 21:17:16 +0900

    Title: 실수 입력받아 계산 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260429/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    float a{0.0f};
+
+    cout.precision(2);
+    cout << fixed;
+
+    cin >> a;
+    cout << a << endl;
+
+    return 0;
+}
