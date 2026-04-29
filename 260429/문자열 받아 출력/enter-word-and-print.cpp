2ceb092c24e2541a164cf217652ce98728a74366
hwshin <30830323+hex0713@users.noreply.github.com>
2026-04-29 22:13:09 +0900

    Title: 문자열 받아 출력, Time: 0ms, Memory: 0MB, Status: Compile Error - Codetree

---
+++ 260429/문자열 받아 출력/enter-word-and-print.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#inclue <string>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    string word;
+
+    cin >> word;
+    cout << word;
+    return 0;
+}
