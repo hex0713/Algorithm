89ae4fbe971504fd2b868d70b9adf5d7049584fc
hwshin <30830323+hex0713@users.noreply.github.com>
2026-04-29 22:07:07 +0900

    Title: 세 실수의 반올림, Time: 0ms, Memory: 0MB, Status: Compile Error - Codetree

---
+++ 260429/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    float a{0},b{0},c{0};
+
+    cout.preicision(3);
+    cout << fixed;
+
+    cin >> a >> b >> c;
+
+    cout << a << endl << b << endl << c;
+    return 0;
+}
