69612ad236caef69ee4b764bf816e8b6e8fc3fd5
hwshin <30830323+hex0713@users.noreply.github.com>
2026-04-30 22:23:16 +0900

    [Passed] 길이 변형 후 사각형 넓이 구하기 (8ms, 0MB)

---
+++ 260430/길이 변형 후 사각형 넓이 구하기/square-width-after-length-change.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int x{0},y{0};
+    cin >> x >> y;
+    x = x + 8;
+    y = y * 3;
+
+    cout << x << endl;
+    cout << y << endl;
+    cout << x*y << endl;
+
+    return 0;
+}
