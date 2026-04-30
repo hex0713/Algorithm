3912565474bc99d3f6aeaa464698cff55d05364c
hwshin <30830323+hex0713@users.noreply.github.com>
2026-04-30 22:28:26 +0900

    [Passed] 몫과 나머지 (8ms, 0MB)

---
+++ 260430/몫과 나머지/quotient-remainder.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int A{0}, B{0};
+
+    cin >> A >> B;
+
+    cout << A/B << "..." << A%B << endl;
+    return 0;
+}
