1f7113ecf4edec995cf6f51f8e70c947be7cc1fa
hwshin <30830323+hex0713@users.noreply.github.com>
2026-04-30 22:36:41 +0900

    [Passed] 입력받아 연산하기 (8ms, 0MB)

---
+++ 260430/입력받아 연산하기/take-input-and-operate.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a{0}, b{0};
+
+    cin >> a >> b;
+
+    a = a + 87;
+    b = b % 10;
+    cout << a << endl;
+    cout << b << endl;
+
+    return 0;
+}
