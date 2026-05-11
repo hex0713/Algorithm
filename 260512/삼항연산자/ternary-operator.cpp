e17c4de444ed963a8e679aa63d2d6e4ae556502c
hwshin <30830323+hex0713@users.noreply.github.com>
2026-05-12 06:57:11 +0900

    [Passed] 삼항연산자 (8ms, 0MB)

---
+++ 260512/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int score{0};
+
+    cin >> score;
+
+    if(score == 100)
+        cout << "pass" << endl;
+    else
+        cout << "failure" << endl;
+
+    
+    return 0;
+}
