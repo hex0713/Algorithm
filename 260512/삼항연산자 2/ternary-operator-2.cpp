cb2df62527083d173f4cb722b20e1bb7137f0158
hwshin <30830323+hex0713@users.noreply.github.com>
2026-05-12 07:04:12 +0900

    [Passed] 삼항연산자 2 (9ms, 0MB)

---
+++ 260512/삼항연산자 2/ternary-operator-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a{0};
+
+    cin >> a;
+
+    cout << (a == 1 ? 't' : 'f') << endl;
+
+
+    return 0;
+}
