baaa5227d404e95d02e7b416e86b459eff687607
hwshin <30830323+hex0713@users.noreply.github.com>
2026-06-02 07:17:36 +0900

    [Passed] 살 수 있는 물건 2 (2ms, 0MB)

---
+++ 260602/살 수 있는 물건 2/things-able-to-buy-2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int N{0};
+
+    cin >> N;
+
+    if(N >= 3000)
+    {
+        cout << "book" << endl;
+    }
+    else if(N >= 1000)
+    {
+        cout << "mask" << endl;
+    }
+    else if(N >= 500)
+    {
+        cout << "pen" << endl;
+    }
+    else
+    {
+        cout << "no" << endl;
+    }
+
+    return 0;
+}
