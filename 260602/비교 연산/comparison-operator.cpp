4013bf1fc9afe0b224c7a4f11e1124026a49d827
hwshin <30830323+hex0713@users.noreply.github.com>
2026-06-02 21:11:50 +0900

    [Passed] 비교 연산 (2ms, 0MB)

---
+++ 260602/비교 연산/comparison-operator.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int A{0}, B{0};
+
+    cin >> A >> B;
+
+    if(A >= B)
+    {
+        cout << true << endl;
+    }
+    else
+    {
+        cout << false << endl;
+    }
+
+    if(A > B)
+    {
+        cout << true << endl;
+    }
+    else
+    {
+        cout << false << endl;
+    }
+
+    if(A <= B)
+    {
+        cout << true << endl;
+    }
+    else
+    {
+        cout << false << endl;
+    }
+
+    if(A < B)
+    {
+        cout << true << endl;
+    }
+    else
+    {
+        cout << false << endl;
+    }
+
+    if(A == B)
+    {
+        cout << true << endl;
+    }
+    else
+    {
+        cout << false << endl;
+    }
+
+    if(A != B)
+    {
+        cout << true << endl;
+    }
+    else
+    {
+        cout << false << endl;
+    }
+
+
+    return 0;
+}
