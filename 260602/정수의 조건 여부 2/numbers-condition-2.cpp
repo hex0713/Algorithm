9bd3f3a20b0f1477f54512783466d2c58e682939
hwshin <30830323+hex0713@users.noreply.github.com>
2026-06-02 20:44:48 +0900

    [Passed] 정수의 조건 여부 2 (2ms, 0MB)

---
+++ 260602/정수의 조건 여부 2/numbers-condition-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a{0};
+
+    cin >> a;
+
+    if(a == 5)
+    {
+        cout << "A" << endl;
+    }
+    else if(a % 2 == 0)
+    {
+        cout << "B" << endl;
+    }
+    
+    return 0;
+}
