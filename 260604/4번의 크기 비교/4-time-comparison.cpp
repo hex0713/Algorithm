32ad31759b9dcf125d5d141b9fc19cfa210edddf
hwshin <30830323+hex0713@users.noreply.github.com>
2026-06-04 21:12:19 +0900

    [Passed] 4번의 크기 비교 (2ms, 0MB)

---
+++ 260604/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int A{0}, B{0}, C{0}, D{0}, E{0};
+
+    cin >> A >> B >> C >> D >> E;
+
+    if(A > B)
+    {
+        cout << true << endl;
+    }
+    else
+    {
+        cout << false << endl;
+    }
+
+    if(A > C)
+    {
+        cout << true << endl;
+    }
+    else
+    {
+        cout << false << endl;
+    }
+
+    if(A > D)
+    {
+        cout << true << endl;
+    }
+    else
+    {
+        cout << false << endl;
+    }
+
+    if(A > E)
+    {
+        cout << true << endl;
+    }
+    else
+    {
+        cout << false << endl;
+    }
+
+    return 0;
+}
