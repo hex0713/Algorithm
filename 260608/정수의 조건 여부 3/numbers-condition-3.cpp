10ce61d522c2bf7a3203ac3eca1667f8af0b0359
hwshin <30830323+hex0713@users.noreply.github.com>
2026-06-08 18:38:31 +0900

    [Passed] 정수의 조건 여부 3 (3ms, 0MB)

---
+++ 260608/정수의 조건 여부 3/numbers-condition-3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a{0};
+
+    cin >> a;
+
+    if(a % 13 == 0 || a % 19 == 0)
+    {
+        cout << "True" << endl;
+    }
+    else
+    {
+        cout << "False" << endl;
+    }
+    
+    return 0;
+}
