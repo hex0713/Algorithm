fbd141940b64acecaab492ad5ff15fa6f14f76ba
hwshin <30830323+hex0713@users.noreply.github.com>
2026-06-12 08:21:30 +0900

    [Passed] 입력받는 수 부터 100까지 출력 (2ms, 0MB)

---
+++ 260612/입력받는 수 부터 100까지 출력/print-number-from-given-num-to-100.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n{0};
+
+    cin >> n;
+
+    for(n; n <= 100; n++)
+    {
+        cout << n << " ";
+    }
+
+    return 0;
+}
