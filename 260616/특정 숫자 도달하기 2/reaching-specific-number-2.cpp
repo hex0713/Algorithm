05b533fe54c5d269faad896e02f0fac9d432b325
hwshin <30830323+hex0713@users.noreply.github.com>
2026-06-16 22:51:13 +0900

    [Passed] 특정 숫자 도달하기 2 (2ms, 0MB)

---
+++ 260616/특정 숫자 도달하기 2/reaching-specific-number-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int N{0};
+
+    cin >> N;
+
+    for(int i = N; i >= 1; i--)
+    {
+        cout << i << " ";
+    }
+
+    return 0;
+}
