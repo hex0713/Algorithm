ff15c60da96a28c1b8a2efef1488276dee4f9a24
hwshin <30830323+hex0713@users.noreply.github.com>
2026-06-16 22:33:03 +0900

    [Passed] 정수 입력받아 배수 출력 (2ms, 0MB)

---
+++ 260616/정수 입력받아 배수 출력/print-multiple-of-input.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int N{0};
+
+    cin >> N;
+
+    for(int i = 1; i <= 5; i++)
+    {
+        cout << N * i << " ";
+    }
+
+
+    return 0;
+}
