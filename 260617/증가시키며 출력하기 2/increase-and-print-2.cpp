55f07a0bf9a9eff0ea59598b76e00011dff9acd4
hwshin <30830323+hex0713@users.noreply.github.com>
2026-06-17 21:00:43 +0900

    [Passed] 증가시키며 출력하기 2 (2ms, 0MB)

---
+++ 260617/증가시키며 출력하기 2/increase-and-print-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n = 10;
+
+    while(n <= 26)
+    {
+        cout << n++ << " ";
+    }
+    return 0;
+}
