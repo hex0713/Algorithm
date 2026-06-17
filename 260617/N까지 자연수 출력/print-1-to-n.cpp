f6ed9f8bbe9d0410e57ddc6278b33c772a9cc7eb
hwshin <30830323+hex0713@users.noreply.github.com>
2026-06-17 22:40:50 +0900

    [Passed] N까지 자연수 출력 (2ms, 0MB)

---
+++ 260617/N까지 자연수 출력/print-1-to-n.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N{0};
+
+    cin >> N;
+    int i = 1;
+
+    while(i <= N)
+    {
+        cout << i++ << " ";
+    }
+    return 0;
+}
