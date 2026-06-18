23b9b33c14fa28e6bb893793e3fa0e2f094edb58
hwshin <30830323+hex0713@users.noreply.github.com>
2026-06-18 23:58:51 +0900

    [Passed] 별 N개 출력하기 (2ms, 0MB)

---
+++ 260618/별 N개 출력하기/print-n-stars.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int N{0};
+
+    cin >> N;
+
+    while(N-- > 0)
+    {
+        cout << "*" << endl;
+    }
+
+    return 0;
+}
