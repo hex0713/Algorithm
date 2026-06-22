dd4c39869c19519c7cf19eb7dd5d4755eb3a48a1
hwshin <30830323+hex0713@users.noreply.github.com>
2026-06-22 23:53:09 +0900

    [Passed] 감소시키며 출력하기 2 (2ms, 0MB)

---
+++ 260622/감소시키며 출력하기 2/decrease-and-print-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int i = 26;
+    while(10 <= i)
+    {
+        cout << i-- << " ";
+    }
+
+    return 0;
+}
