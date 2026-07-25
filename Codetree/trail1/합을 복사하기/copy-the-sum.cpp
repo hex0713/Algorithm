db143416cc3bbd490f1d25db9439fb01d000823b
hwshin <30830323+hex0713@users.noreply.github.com>
2026-07-25 23:22:11 +0900

    [Passed] 합을 복사하기 (2ms, 0MB)

---
+++ Codetree/trail1/합을 복사하기/copy-the-sum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a{1}, b{2}, c{3};
+
+    a = a + b + c;
+    b = a;
+    c = a;
+
+    cout << a << " " << b << " " << c << endl;
+    return 0;
+}
