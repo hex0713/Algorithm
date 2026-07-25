439a209765f2d222a11664b3506ea246483b4de8
hwshin <30830323+hex0713@users.noreply.github.com>
2026-07-25 22:45:32 +0900

    [Passed] 소수점 반올림하기 (2ms, 0MB)

---
+++ Codetree/trail1/소수점 반올림하기/rounding-decimal-points.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    cout.precision(1);
+    cout << fixed;
+
+    float a = 25.352f;
+
+    cout << a << endl;
+
+
+    return 0;
+}
