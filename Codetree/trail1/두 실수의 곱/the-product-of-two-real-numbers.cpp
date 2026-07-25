b352c25325625b583f2b91a55cf12438773ec913
hwshin <30830323+hex0713@users.noreply.github.com>
2026-07-25 22:50:42 +0900

    [Passed] 두 실수의 곱 (2ms, 0MB)

---
+++ Codetree/trail1/두 실수의 곱/the-product-of-two-real-numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    cout << fixed;
+    cout.precision(3);
+
+    float a{5.26f}, b{8.27};
+
+    cout << a * b << endl;
+    return 0;
+}
