d2cb5d2f385626968d4b25b4136d4ed22822055b
hwshin <30830323+hex0713@users.noreply.github.com>
2026-07-25 22:31:16 +0900

    [Passed] 세 정수형 변수 선언 (2ms, 0MB)

---
+++ Codetree/trail1/세 정수형 변수 선언/declaration-of-three-natural-numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a{7}, b{23}, c{30};
+
+    cout << a << " + " << b << " = " << c << endl;
+    return 0;
+}
