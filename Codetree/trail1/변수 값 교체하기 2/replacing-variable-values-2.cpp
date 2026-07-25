92b13621d39721cc5c5817014d1deecd2ba5cf51
hwshin <30830323+hex0713@users.noreply.github.com>
2026-07-25 22:59:06 +0900

    [Passed] 변수 값 교체하기 2 (2ms, 0MB)

---
+++ Codetree/trail1/변수 값 교체하기 2/replacing-variable-values-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a{5}, b{3};
+
+    a = b;
+
+    cout << a << endl;
+    cout << b << endl;
+    
+    return 0;
+}
