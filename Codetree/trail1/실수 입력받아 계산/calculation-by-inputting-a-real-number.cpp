b84d1e35e7839f1891a6fb65e94b66253fa86061
hwshin <30830323+hex0713@users.noreply.github.com>
2026-07-26 23:44:42 +0900

    [Passed] 실수 입력받아 계산 (2ms, 0MB)

---
+++ Codetree/trail1/실수 입력받아 계산/calculation-by-inputting-a-real-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    float a{0},b{0};
+
+    cout.precision(2);
+    cout << fixed;
+
+
+    cin >> a >> b;
+    cout << a + b << endl;
+    
+    return 0;
+}
