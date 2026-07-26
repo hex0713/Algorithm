d175221d1c909a73723c0782bac37b4d1ba2542c
hwshin <30830323+hex0713@users.noreply.github.com>
2026-07-26 23:30:00 +0900

    [Passed] 실수 받아 그대로 출력 (2ms, 1MB)

---
+++ Codetree/trail1/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    float N{0.f};
+
+    cin >> N;
+
+    cout << fixed;
+    cout.precision(2);
+    cout << N << endl;
+    return 0;
+}
