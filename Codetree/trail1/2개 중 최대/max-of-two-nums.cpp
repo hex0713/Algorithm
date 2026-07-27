8c97f7d44e5da95fe5fe54a7a418edb3e9e0b7ff
hwshin <30830323+hex0713@users.noreply.github.com>
2026-07-28 08:00:50 +0900

    [Passed] 2개 중 최대 (2ms, 0MB)

---
+++ Codetree/trail1/2개 중 최대/max-of-two-nums.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int A{0}, B{0};
+
+    cin >> A >> B;
+
+    if(A >= B)
+        cout << A << endl;
+    else
+        cout << B << endl;
+        
+    return 0;
+}
