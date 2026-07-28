c5844929b07204ae5ffebd052344e81055472c3a
hwshin <30830323+hex0713@users.noreply.github.com>
2026-07-28 23:32:09 +0900

    [Passed] 최대 2번의 연산 (2ms, 0MB)

---
+++ Codetree/trail1/최대 2번의 연산/up-to-2-calculations.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a{0};
+
+    cin >> a;
+
+    if(a % 2 == 0)
+    {
+        a /= 2;
+    }
+    
+    if(a % 2 != 0)
+    {
+        a = (a + 1) / 2;
+    }
+
+    cout << a << endl;
+
+    return 0;
+}
